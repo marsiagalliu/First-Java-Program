7dc2eb384660a8b68323e3b5be6b6749ae0b7554
Marsi <agalliumars@gmail.com>
2022-12-11 21:42:00 +0100

    First Java Program

---
+++ Task.java
@@ -0,0 +1,7 @@
+public class Task{
+    public static void main(String[] args) {
+        System.out.println("My name is Marsi Agalliu");
+        System.out.println("I am 22 years old");
+        System.out.println("My hometown is Tirana, Albania");
+    }
+}
